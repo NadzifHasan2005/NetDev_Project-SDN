e1b8c50e7e5bf145a225cdf0aaa1895744914734
NadzifHasan2005 <92029471+NadzifHasan2005@users.noreply.github.com>
2025-05-09 01:06:35 +0700

    QualityofServive(QoS)-Ryu.py

--- Ryu-Cofiguration/QualityofServive(QoS)-Ryu.py
+++ Ryu-Cofiguration/QualityofServive(QoS)-Ryu.py
@@ -1,1 +1,113 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ipv4, tcp, udp
 
+class QoSController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(QoSController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Install default table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 0, match, actions)
+
+        # Setup QoS Queues (S1 and S2)
+        for port in [1, 2, 3]:
+            # High Priority Queue (VoIP - 80%)
+            parser.OFPQueueMod(
+                datapath=datapath,
+                port=port,
+                queue_id=0,
+                min_rate=8000,
+                max_rate=8000
+            )
+            # Low Priority Queue (HTTP - 20%)
+            parser.OFPQueueMod(
+                datapath=datapath,
+                port=port,
+                queue_id=1,
+                min_rate=2000,
+                max_rate=2000
+            )
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # Packet processing with QoS
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        
+        # Skip LLDP packets
+        if eth.ethertype == 0x88cc:
+            return
+
+        # Learn MAC addresses
+        self.mac_to_port.setdefault(datapath.id, {})
+        self.mac_to_port[datapath.id][eth.src] = in_port
+
+        # Determine output port
+        if eth.dst in self.mac_to_port[datapath.id]:
+            out_port = self.mac_to_port[datapath.id][eth.dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        # Set QoS based on traffic type
+        actions = []
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        
+        if ip_pkt:
+            if ip_pkt.proto == 17:  # UDP (VoIP)
+                actions.append(parser.OFPActionSetQueue(0))
+            elif ip_pkt.proto == 6:  # TCP (HTTP)
+                actions.append(parser.OFPActionSetQueue(1))
+        
+        actions.append(parser.OFPActionOutput(out_port))
+
+        # Install flow if not flooding
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(
+                in_port=in_port,
+                eth_dst=eth.dst,
+                eth_type=eth.ethertype
+            )
+            self.add_flow(datapath, 1, match, actions)
+
+        # Send packet out
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data
+        )
+        datapath.send_msg(out)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
