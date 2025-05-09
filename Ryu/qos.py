b3412916a58f3ec801e807dba0fdea37dac91bd8
Yogskiy <146057549+Yogskiy@users.noreply.github.com>
2025-05-09 21:56:31 +0700

    Create QoS Script

---
+++ Ryu/qos.py
@@ -0,0 +1,88 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ipv4, tcp, udp
+
+
+class QoSController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(QoSController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        if eth.ethertype == 0x88cc:
+            return  # Ignore LLDP
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+        self.mac_to_port[dpid][eth.src] = in_port
+
+        out_port = self.mac_to_port[dpid].get(eth.dst, ofproto.OFPP_FLOOD)
+        actions = []
+
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+
+        if ip_pkt:
+            if ip_pkt.proto == 17:  # UDP (VoIP)
+                actions.append(parser.OFPActionSetQueue(0))
+            elif ip_pkt.proto == 6:  # TCP (HTTP)
+                actions.append(parser.OFPActionSetQueue(1))
+
+        actions.append(parser.OFPActionOutput(out_port))
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(
+                in_port=in_port,
+                eth_type=0x0800,
+                ipv4_src=ip_pkt.src,
+                ipv4_dst=ip_pkt.dst,
+                ip_proto=ip_pkt.proto
+            )
+            self.add_flow(datapath, 1, match, actions)
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
+        )
+        datapath.send_msg(out)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
